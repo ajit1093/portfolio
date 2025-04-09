25b7fadf7aeb7c1c77caf4f4fd627e77dbdf2188
ajit1093 <ajitsbabar2@gmail.com>
2025-04-09 12:55:03 +0530

    new changes

--- src/Containers/errorPage/EroorPage.jsx
+++ src/Containers/errorPage/EroorPage.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import './errorPage.scss';
-const ErrorPage=()=>{
-    return(
-        <div className="errorpage"> 
-        <h1>Page Not Found!!!...</h1>
+import { FaExclamationTriangle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./errorPage.scss";
+
+const ErrorPage = () => {
+    return (
+        <div className="not-found-page">
+          <div className="not-found-content">
+            <FaExclamationTriangle className="error-icon" />
+            <h1>404</h1>
+            <p>Oops! The page you're looking for doesn't exist.</p>
+            <Link to="/" className="home-link">
+              Go Back Home
+            </Link>
+          </div>
         </div>
-    )
-}
-export default ErrorPage
+      );
+};
+
+export default ErrorPage;
